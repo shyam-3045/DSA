270ce5bbe3019d002fd0d62b5bfcc5f7a2e368bd
Shyam <s.m.shyam45@gmail.com>
2026-06-09 10:29:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (72.16%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,15 +1,42 @@
 class Solution {
     public int hIndex(int[] c) {
-        Arrays.sort(c);
-        int n=c.length;
+        // Arrays.sort(c);
+        // int n=c.length;
 
-        for(int i=0;i<c.length;i++){
-            if(c[i] >= n-i){
-                return n-i;
+        // for(int i=0;i<c.length;i++){
+        //     if(c[i] >= n-i){
+        //         return n-i;
+        //     }
+        // }
+
+        // return 0 ;
+
+        int n = c.length;
+        int[] count = new int[n+1];
+
+        for(int i=0;i<n;i++){
+            if(c[i] >= n){
+                count[n]++;
+            }
+            else{
+                count[c[i]]++;
+            }
+            
+        }
+
+        int papers =0;
+        
+
+        for(int i=n;i>=0;i--){
+            papers += count[i];
+
+            if(papers >= i){
+
+                return i;
             }
         }
 
-        return 0 ;
+        return 0;
        
     }
 
