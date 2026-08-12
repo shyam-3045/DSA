98f9f6145671a436194d7e086ae62539d6aec0a8
Shyam <s.m.shyam45@gmail.com>
2026-08-12 10:58:57 +0530

    Time: 12 ms (29.20%) | Memory: 47.6 MB (21.16%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -14,12 +14,13 @@ class Solution {
 
         while(r<n)
         {
-            
+            if(hash[s.charAt(r)] != -1)
+            {
                 if(hash[s.charAt(r)] >= l)
                 {
                     l=hash[s.charAt(r)]+1;
                 }
-            
+            }
             
             maxLen=Math.max(maxLen,r-l+1);
             hash[s.charAt(r)]=r;
