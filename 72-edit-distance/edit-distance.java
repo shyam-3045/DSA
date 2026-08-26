62e68f3a21b834ea3c3465b3a85f275475bae7ed
Shyam <s.m.shyam45@gmail.com>
2026-08-26 16:23:29 +0530

    Time: 3 ms (98.24%) | Memory: 47.2 MB (52.63%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,20 @@
+class Solution {
+    private int fn(String s ,String p , int n, int m,int[][] dp)
+    {
+        if(n < 0)return m+1;
+        if(m <0)return n+1;
+        if(dp[n][m] != -1)return dp[n][m];
+
+        if(s.charAt(n) == p.charAt(m)) return dp[n][m]=0+fn(s,p,n-1,m-1,dp);
+
+        return dp[n][m]= Math.min(1+fn(s,p,n-1,m,dp) , Math.min(1+fn(s,p,n,m-1,dp) ,1+fn(s,p,n-1,m-1,dp)));
+    }
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()][word2.length()];
+        for(int[] row : dp)
+        {
+            Arrays.fill(row,-1);
+        }
+        return fn(word1,word2,word1.length()-1,word2.length()-1,dp);
+    }
+}
