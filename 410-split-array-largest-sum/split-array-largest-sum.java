55df3402a03f648afe8e249687abf01eb27f5d58
Shyam <s.m.shyam45@gmail.com>
2026-08-28 19:43:20 +0530

    Time: 165 ms (4.81%) | Memory: 43.7 MB (7.10%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    private int fn(int[] nums,int k,int[] pre,int n,int[][] dp)
+    {
+        if(k==1) return pre[n]+nums[n];
+        if(dp[n][k] != -1) return dp[n][k];
+
+        int ans=Integer.MAX_VALUE;
+        for(int i=k-1;i<=n;i++)
+        {
+            int left = fn(nums,k-1,pre,i-1,dp);
+            int right = pre[n] + nums[n] - pre[i];
+            int lar = Math.max(left,right);
+            ans = Math.min(ans,lar);
+        }
+        return dp[n][k]=ans ;
+    }
+    public int splitArray(int[] nums, int k) {
+        int[] prefix = new int[nums.length];
+        int pre =0 ,sum=0;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            prefix[i] =pre;
+            pre+=nums[i];
+            sum+=nums[i];
+        }
+        int[][] dp = new int[nums.length][k+1];
+        for(int[] row:dp)
+        {
+            Arrays.fill(row,-1);
+        }
+
+        return fn(nums,k,prefix,nums.length-1,dp);
+
+    }
+}
