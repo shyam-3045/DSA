b1c9cee964aa5e5cc615eaf4dc28a02f16e5aa38
Shyam <s.m.shyam45@gmail.com>
2026-08-29 12:12:52 +0530

    Time: 10 ms (23.72%) | Memory: 44.2 MB (15.71%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,35 +1,55 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
+        // HashMap<Character,Integer> map = new HashMap<>();
+        // List<Integer> li = new ArrayList<>();
+        // for(int i =0;i<s.length();i++)
+        // {
+        //     map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        // }
+        // HashMap<Character,Integer> temp = new HashMap<>();
+        // int l =0 , r=0;
+        // while(r<s.length())
+        // {
+        //     char ch = s.charAt(r);
+        //     if(!temp.containsKey(ch)) 
+        //     {
+        //         temp.put(ch ,map.get(ch)-1);
+        //         if(temp.get(ch) == 0) temp.remove(ch);
+        //     }
+        //     else 
+        //     {
+        //         temp.put(ch,temp.get(ch)-1);
+        //         if(temp.get(ch) == 0) temp.remove(ch);
+        //     }
+
+        //     if(temp.isEmpty()) 
+        //     {
+        //         li.add(r-l+1);
+        //         l=r+1;
+        //     }
+        //     r++;
+        // }
+
+        // return li;
+
+
         HashMap<Character,Integer> map = new HashMap<>();
         List<Integer> li = new ArrayList<>();
         for(int i =0;i<s.length();i++)
         {
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+            map.put(s.charAt(i),i);
         }
-        HashMap<Character,Integer> temp = new HashMap<>();
-        int l =0 , r=0;
-        while(r<s.length())
-        {
-            char ch = s.charAt(r);
-            if(!temp.containsKey(ch)) 
-            {
-                temp.put(ch ,map.get(ch)-1);
-                if(temp.get(ch) == 0) temp.remove(ch);
-            }
-            else 
-            {
-                temp.put(ch,temp.get(ch)-1);
-                if(temp.get(ch) == 0) temp.remove(ch);
-            }
+        int l=0,r=0;
 
-            if(temp.isEmpty()) 
+        for(int i=0;i<s.length();i++)
+        {
+            r = Math.max(r,map.get(s.charAt(i)));
+            if(i ==r)
             {
                 li.add(r-l+1);
                 l=r+1;
             }
-            r++;
         }
-
         return li;
 
     }
